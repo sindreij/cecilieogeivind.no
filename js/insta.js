4097cadcdb2ca8b2fde8d66018eb425f1eaf59e4
Sindre Johansen <sindre@iterate.no>
2015-08-23 11:14:50 +0200

    Better instagram

--- js/insta.js
+++ js/insta.js
@@ -2,16 +2,20 @@ function updateImages(data) {
     "use strict";
     console.log(data);
     var element = $('#insta');
+    element.html('');
 
     var elements = _.map(data.data, function(obj) {
-        var div = $('<div></div>');
+        var div = $('<div class="instaimage"></div>');
         var a = $('<a></a>');
         a.attr('href', obj.link);
         a.attr('target', '_blank');
         var img = $('<img></img>');
+        var caption = $('<div class="caption"></div>');
+        caption.text('@' + obj.user.username);
         img.attr('src', obj.images.low_resolution.url);
         a.append(img);
         div.append(a);
+        div.append(caption);
         return div;
     });
 
